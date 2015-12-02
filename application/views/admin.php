34e9e952e3656c9d2197c7ccaf3a91373f66afab
Edgar S. Hurtado <edsanhu@gmail.com>
2015-12-02 19:03:48 +0100

    Implement autoredirect after 3 seconds

--- application/views/admin.php
+++ application/views/admin.php
@@ -2,7 +2,8 @@
  echo "<h1>Hola ". $_SESSION['usuario'] .", Usted sabe qué es lo que me excita</h1>";
 ?>
 <br>
- <img src="https://i.ytimg.com/vi/alukgT3K2-E/hqdefault.jpg">
-
- <a href="<?php echo site_url("login/incidencias")?>">Incidencias</a>
+<img src="https://i.ytimg.com/vi/alukgT3K2-E/hqdefault.jpg">
+<?php
+ header("refresh:3;url=".site_url("login/incidencias"))
+?>
 
