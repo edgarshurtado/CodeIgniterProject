f686c2c41197864eebe0109e06b9c305ed36222a
Edgar S. Hurtado <edsanhu@gmail.com>
2015-12-04 10:27:47 +0100

    Change default controler

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'login';
-$route['(:any)'] = 'login/$1';
+
+$route['default_controller'] = 'front_controller';
+$route['(:any)'] = 'front_controller/$1';
+$route['admin'] = 'login';
+$route['admin/(:any)'] = 'login/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
