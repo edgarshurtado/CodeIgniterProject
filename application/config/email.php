8e569e05000494efc823271776d4bbc445b32037
Edgar S. Hurtado <edsanhu@gmail.com>
2015-12-04 10:24:37 +0100

    Add config email file
    Not configured yet

---
+++ application/config/email.php
@@ -0,0 +1,8 @@
+<?php
+    $config['protocol']="sendmail";
+    $config['smtp_host']="sendmail";
+    $config['protocol']="sendmail";
+    $config['protocol']="sendmail";
+    $config['protocol']="sendmail";
+
+?>
